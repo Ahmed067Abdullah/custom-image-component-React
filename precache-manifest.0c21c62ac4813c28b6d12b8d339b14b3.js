a762f12955b2946297026b3cb7e17188a7451a74
Ahmed067Abdullah <ahmed@sudofy.com>
2020-05-29 23:42:40 +0500

    Updates

---
+++ precache-manifest.0c21c62ac4813c28b6d12b8d339b14b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2375972f13fd44edc9840df6c792c474",
+    "url": "/custom-image-component-React/index.html"
+  },
+  {
+    "revision": "38438db9a49cd43f1488",
+    "url": "/custom-image-component-React/static/css/main.26ad219c.chunk.css"
+  },
+  {
+    "revision": "b48a729a5626fe4032f4",
+    "url": "/custom-image-component-React/static/js/2.429f7f27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/custom-image-component-React/static/js/2.429f7f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38438db9a49cd43f1488",
+    "url": "/custom-image-component-React/static/js/main.a7406696.chunk.js"
+  },
+  {
+    "revision": "4df3d2756a8f792ef27a",
+    "url": "/custom-image-component-React/static/js/runtime-main.fa0d3b08.js"
+  }
+]);
