7e6c9c81795e7c10da197767390b70b6a622ed29
Ahmed067Abdullah <ahmed@sudofy.com>
2020-06-01 22:25:11 +0500

    more props added

--- src/components/Img/Img.js
+++ src/components/Img/Img.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 
-const CustomImg = ({ srcLQ, srcHQ }) => {
+const CustomImg = ({ srcLQ, srcHQ, alt = "", onLoad, runItBefore = false }) => {
   const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleOnImageLoaded = () => {
+    if (onLoad && runItBefore) onLoad();
+    setIsLoaded(true);
+    if (onLoad && !runItBefore) onLoad();
+  }
 
   return srcLQ.trim() && srcHQ.trim()
     ? <>
       <img
         src={srcHQ}
-        alt=""
-        onLoad={() => setIsLoaded(true)}
+        alt={alt}
+        onLoad={handleOnImageLoaded}
         style={{ display: isLoaded ? 'initial' : 'none' }}
       />
       {isLoaded
